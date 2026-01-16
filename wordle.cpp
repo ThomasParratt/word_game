793d845caf894b254c2136fd008200117fe09bcf
ThomasParratt <thomasparratt@googlemail.com>
2026-01-16 10:41:24 +0200

    added red letters

--- wordle.cpp
+++ wordle.cpp
@@ -4,46 +4,46 @@
 int main(void)
 {
     std::string answer = "HELLO";
-    std::vector<std::string> words;
-    int count = 0;
+    std::vector<std::string> words; // will be the vector of 6 words
+    int trys = 0;
     
     for (int i = 0; i < 6; i++)
     {
-        words.push_back("_____");
+        words.push_back("_____"); // initialise all words as blank to start
     }
     std::cout << std::endl;
     for (int i = 0; i < 6; i++)
     {
-        std::cout << words[i] << std::endl;
+        std::cout << words[i] << std::endl; // print all blank words
     }
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 6; i++) // looping through trys
     {
-        count++;
+        trys++;
         std::cout << std::endl;
         std::cout << "Attempt " << i + 1 << ": " << std::endl;
         std::string attempt;
-        std::cin >> attempt;
+        std::cin >> attempt; // take word input
         //validity checks for valid guess e.g. length == 5
         std::cout << std::endl;
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 6; i++) // looping through words
         {
-            if (i < count)
+            if (i < trys)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 5; j++) // looping through characters
                 {
                     if (attempt[j] == answer[j])
                     {
-                        if (i == count - 1)
-                            words[i][j] = attempt[j];
-                        std::cout << "\033[32m" << words[i][j] << "\033[0m";
+                        if (i == trys - 1) // so that we only update the word on the current try
+                            words[i][j] = attempt[j]; // updates the current word to contain the guessed characters
+                        std::cout << "\033[32m" << words[i][j] << "\033[0m"; // prints the corrected guess characters
                     }
                     else
                     {
-                        if (i == count - 1)
-                            words[i][j] = '_';
-                        std::cout << words[i][j];
+                        if (i == trys - 1)
+                            words[i][j] = attempt[j]; // updates the current word to contain the guessed characters
+                        std::cout << "\033[31m" << words[i][j] << "\033[0m"; // prints the corrected guess characters
                     }
                 }
                 std::cout << std::endl;
